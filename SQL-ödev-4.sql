f6219951d121ef145cde76fa3ed241a05832ce60
Sailorac <119450113+Sailorac@users.noreply.github.com>
2022-12-02 21:18:30 +0300

    Add files via upload

---
+++ SQL-ödev-4.sql
@@ -0,0 +1,13 @@
+--SELECT DISTINCT replacement_cost FROM film
+
+
+--SELECT COUNT(DISTINCT replacement_cost) FROM film
+
+
+--SELECT  COUNT (title)  FROM film
+--where title like 'T%' and rating = 'G';
+
+--SELECT COUNT (country like '_____') FROM country;
+
+
+--SELECT COUNT (city like 'R%r') FROM city;
